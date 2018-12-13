3b75f12a54dad2938994125ddfc2bf1917abf743
fengshuan <1263215592@qq.com>
2018-12-13 23:29:52 +0800

    feat:less编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 const gulp = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
 
-gulp.task('default', () => {
-    console.log('default task');
+gulp.task('clean', (done) => {
+    del.sync('build');
+    done();
 })
+
+gulp.task('less', (done) => {
+    gulp.src('src/**/*.less')
+    .pipe(less())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('build'));
+    done();
+})
+gulp.task('default', gulp.series('clean','less',(done) => {
+    done();
+}));
