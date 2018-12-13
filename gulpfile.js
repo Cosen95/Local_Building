e185888e56c38dda6d5c064eb82f6fad8392fb67
fengshuan <1263215592@qq.com>
2018-12-13 23:48:17 +0800

    feat:gulp watch监听

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('clean', (done) => {
     del.sync('build');
@@ -15,9 +16,17 @@ gulp.task('less', (done) => {
         browsers: ['last 2 versions'],
         cascade: false
     }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('build'));
     done();
 })
 gulp.task('default', gulp.series('clean','less',(done) => {
     done();
 }));
+
+gulp.task('watch', () => {
+    const watcher = gulp.watch('src/**/*',gulp.series('default'));
+    watcher.on('change', event => {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    })
+})
